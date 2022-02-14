523edd024604dc5333ae87838b98e40a5568dc23
Robin Jidegren <robin_jide@hotmail.com>
2022-02-14 10:31:56 +0100

    First commit

---
+++ FlappyBird.Web/Models/BirdModel.cs
@@ -0,0 +1,25 @@
+﻿
+namespace FlappyBird.Web.Models
+{
+    public class BirdModel
+    {
+        public int DistanceFromGround { get; private set; } = 100;
+        public int JumpStrength { get; private set; } = 50;
+
+        public bool IsOnGround()
+        {
+            return DistanceFromGround <= 0;
+        }
+        public void Fall(int gravity)
+        {
+            DistanceFromGround -= gravity;
+        }
+        public void Jump()
+        {
+            if(DistanceFromGround <= 530)
+            DistanceFromGround += JumpStrength;
+        }
+
+    }
+    
+}
